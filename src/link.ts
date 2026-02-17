a1683dd1a302578729a894edf368b73631397726
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-02-17 15:20:41 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-02-15/images/reddit.jpg";
-const TITLE = "Shipped OpenAI-compatible API endpoint and App Showcase UI updates";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-02-16/images/reddit.jpg";
+const TITLE = "Shipped Pollen badges, video consistency tools, and 20 ecosystem updates";
 export {LINK, TITLE};
