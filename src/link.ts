7dfd4547e848513ebe4dc3067217234d8487c739
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-02-19 15:22:36 +0000

    Deploy updated link.ts and main.ts

--- src/link.ts
+++ src/link.ts
@@ -1,3 +1,3 @@
-const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-02-17/images/reddit.jpg";
-const TITLE = "Shipped SlidePainter, fixed deployment targets, and prettified app descriptions";
+const LINK = "https://raw.githubusercontent.com/pollinations/pollinations/news/social/news/daily/2026-02-18/images/reddit.jpg";
+const TITLE = "Shipped build diary, cost simulator, and better upstream error mapping";
 export {LINK, TITLE};
